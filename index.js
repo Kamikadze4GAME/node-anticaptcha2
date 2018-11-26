add2b47e709ea41c5cf91d198410dad1a13d9b7a
Ostap Skryshevskyi <osskrisha@gmail.com>
2018-11-26 14:51:26 +0200

    Some fixes and improves

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const Tasks = require('./lib/tasks');
+const Anticaptcha = require('./lib/anticaptcha');
+const Proxy       = require('./lib/proxy');
+const Tasks       = require('./lib/tasks');
+const FormBuilder = require('./lib/formBuilder');
+const Errors     = require('./lib/errors');
 
-module.exports = require('./lib/anticaptcha');
-
-module.exports.Proxy = require('./lib/proxy');
-
-module.exports.FormBuilder = require('./lib/formBuilder');
+module.exports             = Anticaptcha;
+module.exports.Anticaptcha = Anticaptcha;
+module.exports.Proxy       = Proxy;
+module.exports.Errors      = Errors;
+module.exports.FormBuilder = FormBuilder;
 
 Object.keys(Tasks).forEach(task => {
   module.exports[task] = Tasks[task];
--- tests/test_1.js
+++ tests/test_1.js
@@ -1,30 +1,23 @@
 'use strict';
 
 const Anticaptcha = require('..');
+const Proxy = Anticaptcha.Proxy;
 
-let a = new Anticaptcha('a33c8bc33086ecae2bc285fb9e8bdae3');
 
-let task = new Anticaptcha.NoCaptchaTask({url:'https://www.boards.ie/', sitekey: '6LfBixYUAAAAABhdHynFUIMA_sa4s-XsJvnjtgB0'});
-// a.test({a:'wefbhjnk'})
-// a.getBalance()
-// a.getQueueStats(1)
-// a.getTaskResult(70254143)
 
-// let taskId = 0;
-// a.createTask({
-//   task: {
-//     type: 'NoCaptchaTaskProxyless',
-//     websiteURL: 'https://www.boards.ie/b/',
-//     websiteKey: '6LfBixYUAAAAABhdHynFUIMA_sa4s-XsJvnjtgB0',
-//     // 70254143
-//   }
-// })
-// .then(res => {
-//   console.log(res);
-//   taskId = res.taskId;
-//   console.log(new Date(), 'taskId', taskId);
-//   return a.getTaskResult2(taskId);
-// })
+
+let proxy = new Proxy({
+  type: 'http',
+  address: '31.202.121.62',
+  port: '36156',
+  agent: 'XUI'
+});
+
+let a = new Anticaptcha('c4fbd67246c4b57dc89c2c3187ae2e71');
+
+let task = new Anticaptcha.NoCaptchaTask({url:'https://www.boards.ie/', sitekey: '6LfBixYUAAAAABhdHynFUIMA_sa4s-XsJvnjtgB0'}, proxy);
+
+// a.getTaskResult(122284760, {extended: false, wait:true})
 a.solve(task)
 .then(_ => {
   console.log('res', _);
